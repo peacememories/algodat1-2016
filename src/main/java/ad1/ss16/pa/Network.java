186a4259f227c89c7a6c889ebeebf14919907ee3
Gabriel Pickl <peacemotion@gmail.com>
2016-05-13 17:02:38 +0200

    Implemented working application.

--- src/main/java/ad1/ss16/pa/Network.java
+++ src/main/java/ad1/ss16/pa/Network.java
@@ -6,8 +6,27 @@ public class Network {
 
     private final int numNodes;
 
+    private int numConnections;
+    private int numComponents;
+    private Set<Integer> critical;
+    private boolean hasCycle;
+
+    private final Node nodes[];
+
+    private Status status;
+
+
+    private Queue<Node> unvisited;
+
     public Network(int n) {
         numNodes = n;
+        nodes = new Node[numNodes];
+        for(int i = 0; i < n; i++) {
+            nodes[i] = new Node(i);
+        }
+        reset();
+        numConnections = 0;
+        status = Status.INIT;
     }
 
     public int numberOfNodes() {
@@ -15,40 +34,182 @@ public class Network {
     }
 
     public int numberOfConnections() {
-        return 0;
+        return numConnections;
     }
 
     public void addConnection(int v, int w){
-
+        if(nodes[v].adj.add(nodes[w])) {
+            numConnections++;
+            if(status != Status.INIT) {
+                reset();
+            }
+        }
+        nodes[w].adj.add(nodes[v]);
     }
 
     public void addAllConnections(int v){
-
+        for(int w = 0; w < numNodes; w++) {
+            if(v != w) {
+                addConnection(v, w);
+            }
+        }
     }
 
     public void deleteConnection(int v, int w){
-
+        if(nodes[v].adj.remove(nodes[w])) {
+            numConnections--;
+            if(status != Status.INIT) {
+                reset();
+            }
+        }
+        nodes[w].adj.remove(nodes[v]);
     }
 
     public void deleteAllConnections(int v){
+        for(int w = 0; w < numNodes; w++) {
+            if(v != w) {
+                deleteConnection(v, w);
+            }
+        }
+    }
 
+    private void tarjan() {
+        status = Status.STARTED;
+        while(!unvisited.isEmpty()) {
+            Node start = unvisited.peek();
+            start.level = 0;
+            dfs(start);
+            numComponents++;
+        }
+        status = Status.DONE;
+    }
+
+    private void dfs(Node n) {
+        unvisited.remove(n);
+        n.component = numComponents;
+        n.levelNew = n.level;
+        int numSeparate = 0;
+        for(Node m : n.adj) {
+            if(n.level > 0 && m.level == n.level-1) {
+                continue;
+            }
+            if(m.level == -1) {
+                m.level = n.level + 1;
+                dfs(m);
+            }
+            n.levelNew = Math.min(n.levelNew, m.levelNew);
+            if(m.levelNew > n.level) {
+                numSeparate++;
+            } else {
+                hasCycle = true;
+            }
+        }
+        if(n.level > 0 && n.levelNew >= n.level) {
+            numSeparate++;
+        }
+        if(numSeparate > 0 && n.adj.size() > 1) {
+            critical.add(n.id);
+        }
+    }
+
+    private int pathsearch(Node start, Node end) {
+        Queue<Node> nq = new LinkedList<>();
+        Queue<Integer> dq = new LinkedList<>();
+        Set<Node> visited = new HashSet<>();
+        nq.add(start);
+        nq.add(null);
+        dq.add(0);
+        while(!nq.isEmpty()) {
+            Node n = nq.poll();
+            Node parent = nq.poll();
+            visited.add(n);
+            int depth = dq.poll();
+            for(Node m : n.adj) {
+                if(m == parent) {
+                    continue;
+                }
+                if(m == end) {
+                    return depth+1;
+                } else {
+                    nq.add(m);
+                    nq.add(n);
+                    dq.add(depth+1);
+                }
+            }
+        }
+        return -1;
     }
 
     public int numberOfComponents() {
-        return 0;
+        if(status != Status.DONE) {
+            tarjan();
+        }
+        return numComponents;
     }
 
     public boolean hasCycle() {
-        return false;
+        if(!hasCycle && status != Status.DONE) {
+            tarjan();
+        }
+        return hasCycle;
     }
 
     public int minimalNumberOfConnections(int start, int end){
-        return 0;
+        if(start == end) {
+            return 0;
+        }
+
+        Node startNode = nodes[start];
+        Node endNode = nodes[end];
+        if(startNode.component == -1) {
+            status = Status.STARTED;
+            startNode.level = 0;
+            dfs(startNode);
+            numComponents++;
+        }
+        if(startNode.component == endNode.component) {
+            return pathsearch(startNode, endNode);
+        } else {
+            return -1;
+        }
     }
 
     public List<Integer> criticalNodes() {
-        List<Integer> critical = new LinkedList<Integer>();
-        return critical;
+        if(status != Status.DONE) {
+            tarjan();
+        }
+        return new ArrayList<>(critical);
+    }
+
+    private void reset() {
+        numComponents = 0;
+        hasCycle = false;
+        critical = new HashSet<>();
+        status = Status.INIT;
+        unvisited = new LinkedList<>(Arrays.asList(nodes));
+        for(Node n : nodes) {
+            n.level = -1;
+            n.levelNew = -1;
+            n.component = -1;
+        }
+    }
+
+    private class Node {
+        private final int id;
+        private int level;
+        private int levelNew;
+        private int component;
+        private Set<Node> adj = new HashSet<>();
+
+        public Node(int id) {
+            this.id = id;
+        }
+    }
+
+    private enum Status {
+        INIT,
+        STARTED,
+        DONE
     }
 }
 
